4e6871a8ab7b48aa30e80bf40b10354dbeee7cfe
DataNick <DataNick@users.noreply.github.com>
2014-11-05 14:53:00 -0500

    destroy loop seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Picture.destroy_all
 
 Picture.create!(
   :title  => "The old church on the coast of White sea",
